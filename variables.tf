44f840dadf620b42d1da47fc9ca4b77191c262c5
Aquib Javed <aquibjaved.devops@gmail.com>
2026-04-22 15:11:03 +0530

    fix: add variable defaults + simplify terraform plan in CI

--- variables.tf
+++ variables.tf
@@ -1,23 +1,41 @@
-variable "region" {}
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
 
-variable "ami_id" {}
+variable "ami_id" {
+  description = "AMI ID for EC2 instance (Amazon Linux 2023)"
+  type        = string
+  default     = "ami-098e39bafa7e7303d"
+}
 
-variable "instance_type" {}
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.micro"
+}
 
 variable "vpc_cidr" {
-  default = "10.0.0.0/16"
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
 }
 
 variable "create_vpc" {
-  default = true
+  description = "Whether to create a new VPC"
+  type        = bool
+  default     = true
 }
 
 variable "log_group_name" {
   description = "Name of the application log group"
   type        = string
+  default     = "devops-agent-app-logs"
 }
 
 variable "os_log_group_name" {
   description = "Name of the OS-level log group"
   type        = string
+  default     = "devops-agent-os-logs"
 }
